1a9c08a7ea1631f727f77530d9f227fc7a78b1c1
Gangyi Sun <gangyi.sun@wustl.edu>
2018-02-10 17:44:54 -0600

    Wrote all methods for S4 class system.

--- HW4(PS3).R
+++ HW4(PS3).R
@@ -22,18 +22,63 @@ PlayGame<-function(x){
 PlayGame.door<-function(x){
   playerDoor<-as.numeric(x)
   carDoor<-sample(1:3,size=1)
+  print(paste("You chose door ", playerDoor, ""))
   print(paste("The car was hidden behind door ", carDoor, ""))
   if (playerDoor==carDoor){
-    print("Congratulations! You choose the correct door!")
+    print("Congratulations! You chose the correct door!")
   } else {
     print ("I'm sorry, you didn't choose the correct door :(")
   }
 }
 
 # To test if S3 methods work
-door1<-createDoor(3)
-door2<-createDoor(4)
-PlayGame(door1)
+door1S3<-createDoor(3)
+door2S3<-createDoor(4)
+PlayGame(door1S3)
+
+
+
+# Part 2: using S4 class system. 
+# Fuctions to define the S4 class door and the create S4 door objects. 
+setClass(Class="door",
+         representation=representation(DoorChoice="integer"), 
+         prototype=prototype(DoorChoice=c())
+         )
+setMethod("initialize", "door",  function(.Object, ...){
+  value = callNextMethod()
+  return(value)
+  }
+  )
+
+# Validation function 
+setValidity("door", function(object){
+  test<-is.integer(object@DoorChoice)
+  if(test!=TRUE){return("@DoorChoice is not a valid value")}
+  }
+  )
+
+# PlayGame method
+setGeneric("PlayGame", function(object="door"){
+             standardGeneric("PlayGame")
+           }
+           )
+setMethod("PlayGame", "door",
+          function(object){
+            playerDoor<-object@DoorChoice
+            carDoor<-sample(1:3,size=1)
+            print(paste("You chose door ", playerDoor, ""))
+            print(paste("The car was hidden behind door ", carDoor, ""))
+            if (playerDoor==carDoor){
+              print("Congratulations! You chose the correct door!")
+            } else {
+              print ("I'm sorry, you didn't choose the correct door :(")
+            }
+          }
+          )
+
+# To test if S4 methods work
+door1S4<-new("door", DoorChoice=as.integer(2))  # explain why as.integer is needed
+PlayGame(door1S4)
 
 
 
