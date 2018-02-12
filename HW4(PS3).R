5b3406d5e057a3945b88cba86c287bc6a8c6e0ff
Gangyi Sun <gangyi.sun@wustl.edu>
2018-02-12 15:22:34 -0600

    Added comments to code. Problem set completed.

--- HW4(PS3).R
+++ HW4(PS3).R
@@ -6,8 +6,8 @@
 # Part 1: using S3 class system. 
 # Function that creates objects of class door
 createDoor<-function(x){
-  if (x==1|x==2|x==3){
-    class(x)<-"door"
+  if (x==1|x==2|x==3){    # ensures that numeric value is 1, 2 or 3
+    class(x)<-"door"      # defines object as class 'door'
     return(x)
   } else {
     print("invalid door")
@@ -17,16 +17,16 @@ createDoor<-function(x){
 
 # Create method PlayGame
 PlayGame<-function(x){
-  UseMethod('PlayGame',x)     
+  UseMethod('PlayGame',x)       # creates generic PlayGame
 }
-PlayGame.door<-function(x){
-  playerDoor<-as.numeric(x)
-  carDoor<-sample(1:3,size=1)
+PlayGame.door<-function(x){     # creates method for class door 
+  playerDoor<-as.numeric(x)     # obtains numeric vale stored in 'door' object 
+  carDoor<-sample(1:3,size=1)   # randomly chooses door behind which the car is hidden 
   print(paste("You chose door ", playerDoor, ""))
   print(paste("The car was hidden behind door ", carDoor, ""))
-  if (playerDoor==carDoor){
+  if (playerDoor==carDoor){     # player chose correct door, congratulate her
     print("Congratulations! You chose the correct door!")
-  } else {
+  } else {                      # player chose wrong door, express sympathy 
     print ("I'm sorry, you didn't choose the correct door :(")
   }
 }
@@ -40,17 +40,19 @@ PlayGame(door1S3)
 
 # Part 2: using S4 class system. 
 # Fuctions to define the S4 class door and the create S4 door objects. 
+# Begin by defining the class 'door'. Objects of class door contain an integer called DoorChoice. 
 setClass(Class="door",
          representation=representation(DoorChoice="integer"), 
          prototype=prototype(DoorChoice=c())
          )
+# Then, write the construction function that allows the user to create a door object 
 setMethod("initialize", "door",  function(.Object, ...){
   value = callNextMethod()
   return(value)
   }
   )
 
-# Validation function 
+# Validation function that checks whether the value stored in door is an integer 
 setValidity("door", function(object){
   test<-is.integer(object@DoorChoice)
   if(test!=TRUE){return("@DoorChoice is not a valid value")}
@@ -58,26 +60,32 @@ setValidity("door", function(object){
   )
 
 # PlayGame method
-setGeneric("PlayGame", function(object="door"){
+# creates generic PlayGame
+setGeneric("PlayGame", function(object="door"){   
              standardGeneric("PlayGame")
            }
            )
+# creates method for class door 
 setMethod("PlayGame", "door",
           function(object){
-            playerDoor<-object@DoorChoice
-            carDoor<-sample(1:3,size=1)
+            playerDoor<-object@DoorChoice     # obtains integer vale stored in 'door' object 
+            carDoor<-sample(1:3,size=1)       # randomly chooses door behind which the car is hidden 
             print(paste("You chose door ", playerDoor, ""))
             print(paste("The car was hidden behind door ", carDoor, ""))
-            if (playerDoor==carDoor){
+            if (playerDoor==carDoor){         # player chose correct door, congratulate her
               print("Congratulations! You chose the correct door!")
-            } else {
+            } else {                          # player chose wrong door, express sympathy 
               print ("I'm sorry, you didn't choose the correct door :(")
             }
           }
           )
 
 # To test if S4 methods work
-door1S4<-new("door", DoorChoice=as.integer(2))  # explain why as.integer is needed
+door1S4<-new("door", DoorChoice=as.integer(2))  # By default, R takes a number (such as the value 2 in 
+                                                # this case) as a numeric. Thus, because the construction
+                                                # function of a S4 object door requires an integer as its
+                                                # content, as.integer() is needed to cast an entered numeric
+                                                # value (2 in this case) as a integer. 
 PlayGame(door1S4)
 
 
